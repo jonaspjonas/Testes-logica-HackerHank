7263f443f63943e157976611a06ca35060b32ec8
jonaspjonas <jonaspjonas@gmail.com>
2021-06-08 21:24:38 -0300

    profissionais incríveis

---
+++ contagem_regressiva.js
@@ -0,0 +1,24 @@
+function solucao(numero) {
+	//seu codigo aqui
+    while (numero >= 0 ) {
+        console.log(numero);
+        numero --;
+    }
+    console.log("KABUM");
+    
+}
+
+function processData(input) {
+	solucao(parseInt(input, 10));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
